49988da920da87382ba47f1db3cd04c88c3b2ae2
Ashleigh Plummer <ashleigh@sportseasy.co.uk>
2020-08-08 20:37:14 +0100

    Second commit.

--- src/Card/manager.ts
+++ src/Card/manager.ts
@@ -1,10 +1,18 @@
 import Task from ".";
+import User from "../User";
 
 class CardManager {
     private tasks: Task[];
+    private user: User;
 
-    constructor(tasks: Task[]){
+    constructor(tasks: Task[], user: User){
         this.tasks = tasks;
+        this.user = user;
+    }
+
+    addTask(title: string, description?:string) {
+        let task = Task()
+        return title;
     }
 
 }
--- src/Task/manager.tsx
+++ src/Task/manager.tsx
@@ -1,4 +1,51 @@
+import User from "../User";
+
 class TaskManager {
+    private _title: string = "";
+    private _position: number = ;
+    private _description: string;
+    private _createdBy: User;
+    private _assignedTo: User;
+
+    get position(): number {
+        return this._position;
+    }
+
+    set position(value: number) {
+        this._position = value;
+    }
+
+    get description(): string {
+        return this._description;
+    }
+
+    set description(value: string) {
+        this._description = value;
+    }
+
+    get createdBy(): User {
+        return this._createdBy;
+    }
+
+    set createdBy(value: User) {
+        this._createdBy = value;
+    }
+
+    get assignedTo(): User {
+        return this._assignedTo;
+    }
+
+    set assignedTo(value: User) {
+        this._assignedTo = value;
+    }
+    get title(): string {
+        return this._title;
+    }
+
+    set title(value: string) {
+        this._title = value;
+    }
+
 
 }
 
